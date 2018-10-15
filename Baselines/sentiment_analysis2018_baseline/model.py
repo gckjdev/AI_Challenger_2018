cafbe0e1583be88927a886b87dc943739128eb82
Benson Peng <penglingzhe@gmail.com>
2018-10-15 15:37:49 +0800

    add some codes

--- Baselines/sentiment_analysis2018_baseline/model.py
+++ Baselines/sentiment_analysis2018_baseline/model.py
@@ -48,6 +48,23 @@ class TextClassifier():
     def get_f1_score(self, x, y):
         return f1_score(y, self.predict(x), average='macro')
 
+# class Metrics(Callback):
+#     def on_train_begin(self, logs={}):
+#         self.val_f1s = []
+#         self.val_recalls = []
+#         self.val_precisions = []
+
+#     def on_epoch_end(self, epoch, logs={}):
+#         val_predict = (numpy.asarray(self.model.predict(
+#             self.validation_data[0]))).round()
+#         val_targ = self.validation_data[1]
+#         _val_f1 = f1_score(val_targ, val_predict)
+#         _val_recall = recall_score(val_targ, val_predict)
+#         _val_precision = precision_score(val_targ, val_predict)
+#         self.val_f1s.append(_val_f1)
+#         self.val_recalls.append(_val_recall)
+#         self.val_precisions.append(_val_precision)
+#         return
 
 def buildRNNModel(input_dim, embedding_weights, num_class):   # input dim in general is vocab len + 1
 
@@ -68,6 +85,9 @@ def buildRNNModel(input_dim, embedding_weights, num_class):   # input dim in gen
     return model
 
 def trainRNNModel(model, content, label, name, epochs):
+
+    # metrics = Metrics()
+
     logger.info("start to train....")
     train = pad_sequences(content, dtype='float32')
     model.fit(train, label, batch_size = 64, epochs = epochs, verbose = 1)       # epochs to be optimized
